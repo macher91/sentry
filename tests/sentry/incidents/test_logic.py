b9427cead575437c1be96e19b1ace09d97b9e63d
Dan Fuller <dfuller@sentry.io>
2020-05-12 18:33:47 -0700

    refs(subscriptions): Use more fields from `snuba_query` to replace fields on the alert rule.
    This continues the work from https://github.com/getsentry/sentry/pull/18751. This converts most of
    the fields in Sentry to use this new model. Will follow this up with a pr to remove the old fields.

--- tests/sentry/incidents/test_logic.py
+++ tests/sentry/incidents/test_logic.py
@@ -661,12 +661,10 @@ class CreateIncidentSnapshotTest(TestCase, BaseIncidentsTest):
 @freeze_time()
 class BulkGetIncidentStatsTest(TestCase, BaseIncidentsTest):
     def test(self):
-        closed_incident = create_incident(
+        closed_incident = self.create_incident(
             self.organization,
-            IncidentType.ALERT_TRIGGERED,
-            "Closed",
-            "",
-            QueryAggregations.TOTAL,
+            title="Closed",
+            query="",
             groups=[self.group],
             date_started=timezone.now() - timedelta(days=30),
         )
@@ -675,12 +673,10 @@ class BulkGetIncidentStatsTest(TestCase, BaseIncidentsTest):
             IncidentStatus.CLOSED,
             status_method=IncidentStatusMethod.RULE_TRIGGERED,
         )
-        open_incident = create_incident(
+        open_incident = self.create_incident(
             self.organization,
-            IncidentType.ALERT_TRIGGERED,
-            "Open",
-            "",
-            QueryAggregations.TOTAL,
+            title="Open",
+            query="",
             groups=[self.group],
             date_started=timezone.now() - timedelta(days=30),
         )
